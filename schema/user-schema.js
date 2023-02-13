794c1f6032e1264e07cd048c0b5ec5a4b7b3a479
Angela Diaz <angeldi846@gmail.com>
2023-02-13 11:10:25 -0500

    update project schema

--- schema/user-schema.js
+++ schema/user-schema.js
@@ -1,22 +1,6 @@
 import mongoose from "mongoose";
 
 const projectSchema = mongoose.Schema({
-    // project_title:String,
-    // project_location:String,
-    // project_duration:String,
-    // project_budget:String,
-    // intervention_sector:String,
-    // imagePath:String,
-    // problematic_summary:String,
-    // beneficiaries:String,
-    // executive_summary:String,
-    // alignment:String,
-    // methodology_summary:String,
-    // general_objetive:String,
-    // specific_objectives:Array,
-    // experience:String,
-    // sustainability:String,
-    // exit_strategy:String
 
     project_title:{
         type: String,
