26e4c8809d6cfc0bbc4031bea17c0e300a185843
Jlbejarano662 <jasmin.1207@hotmail.com>
2023-02-20 19:13:52 -0500

    merge Angelus

---
+++ src/schema/LogonAttempts.js
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+const logonAttemptsSchema = new Schema(
+  {
+    user: {
+      type: String,
+      required: true,
+    },
+    counter: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default model("LogonAttempts", logonAttemptsSchema);
