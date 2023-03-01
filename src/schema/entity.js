a0819cd5f43a1a1e2584d83dcc9e8442a25f4f49
ivanmunoz99 <ivandariomunos99@gmail.com>
2023-03-01 10:56:50 -0500

    schema

--- src/schema/entity.js
+++ src/schema/entity.js
@@ -1,7 +1,26 @@
 const mongoose = require("mongoose");
 
 const dataSchema = mongoose.Schema({
-    
+    contact_email:{
+        type: String,
+        required: true
+    },
+    legal_status:{
+        type: String,
+        required: true
+    },
+    country:{
+        type: String,
+        required: true
+    },
+    foundaion_year:{
+        type: String,
+        required: true
+    },
+    registry_nit:{
+        type: Number,
+        required: true
+    }
 })
 
 module.exports = mongoose.model('data', dataSchema);
