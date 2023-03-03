d660599eec138a742b93d22bf229a878e37fca20
Angela Diaz <angeldi846@gmail.com>
2023-03-03 10:46:37 -0500

    storage update

--- src/libs/storage.js
+++ src/libs/storage.js
@@ -3,7 +3,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./storage/imgs");
+    cb(null, "./src/storage/imgs");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
