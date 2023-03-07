59547ad6e027c951b865ac66c09c15eea22c9bdd
Rabm1504 <114516218+Rabm1504@users.noreply.github.com>
2023-03-07 12:16:11 -0500

    Update project.controller.js

--- src/controller/project.controller.js
+++ src/controller/project.controller.js
@@ -64,3 +64,12 @@ export const deleteProject = async (request, response) => {
         response.status(409).json({message: e.message});
     }
 }
+
+export const viewUser = async (request, response) =>{
+    try {
+        const projects = await Project.find({});
+        response.status(200).json(projects);
+    } catch (e) {
+        response.status(404).json({message: e.message});
+    }
+}
