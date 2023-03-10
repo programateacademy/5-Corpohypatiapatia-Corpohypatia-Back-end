88710afebf6cfd58372115f579f709a7e66ec733
Rabm1504 <rennyangel2@gmail.com>
2023-03-10 15:45:21 -0500

    9

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import express from "express";
 import cors from "cors";
 
 import { createRoles } from "./libs/initialSetup.js";
-import taskBudget from "./routes/task.budget.js";
-import totalBudget from "./routes/total.budget.js";
 import authRoutes from "./routes/auth.routes.js";
 import homeRoutes from "./routes/home.routes.js";
 import projectRoutes from "./routes/project.routes.js";
 import allProjects from "./routes/all.project.js";
-import RoutesUsers from './routes/routeUser.js'
+import RoutesUsers from './routes/route.user.js';
 import entidadroutes from "./controller/form.controller.js"
+
 const app = express();
 await createRoles();
-
 
 app.use(express.json({ exteng: true }));
 app.use(express.urlencoded({ extended: false }));
@@ -32,7 +30,5 @@ app.use("/", authRoutes);
 app.use("/project", projectRoutes);
 app.use("/users/", RoutesUsers);
 app.use("/form", entidadroutes);
-app.use("/task", taskBudget);
-app.use("/total", totalBudget);
 
 export default app;
